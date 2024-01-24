80108529925272e76033fda36ce2c1df9ae35561
Ryan Rasuman <ryan.rasuman@outlook.com>
2024-01-24 10:05:33 +0800

    Added new Speed Insights of Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import Navbar from '@/app/components/navbar/navbar'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
